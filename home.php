7a68ee154dcac64b546a2242192657993e94b4f3
Anila-Dwi-Lestari <aniladwilestariii@gmail.com>
2020-11-25 12:45:47 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,5 @@
+<center><br><br><br><br><br><br><br><br>
+<img src="assets/images/telkom-bandung.png" width="200px" height="200px" /> <br>
+<font Size="6" face="Helvetica">Sistem Informasi Akademik Telkom University</font> <br>
+<font Size="6">Universitas Telkom Bandung</font>
+</center>
